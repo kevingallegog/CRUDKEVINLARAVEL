697743401b3847cc539e3caa547890abda4fba6c
kevingallegog <kgallegog@miumg.edu.gt>
2022-09-03 23:37:48 -0600

    CRUD KEVIN

---
+++ CRUDLaravelKevinG/resources/views/tarea/show.blade.php
@@ -0,0 +1,36 @@
+@extends('tema.app')
+
+@section('title',"Tarea")
+
+@section('contenido')
+    <h3>
+        <i> {{  $tarea->nombre }}</i>
+    </h3>
+    <ul>
+        <li>
+            Finalizada: <strong> {{ $tarea->estaFinalizada() }} </strong>
+        </li>
+        <li>
+            Urgencia: <strong> {{ $tarea->urgencia() }} </strong>
+        </li>
+        <li>
+            Fecha limite: <strong> {{ $tarea->fecha_limite->format('H:i d / m / Y') }} </strong>
+        </li>
+        
+    </ul>
+    <p>
+        {{ $tarea -> descripcion }}
+    </p>
+    <hr>
+    <div class="row">
+        <div class="col-sm12 mb-2">
+            <form action="{{ route('tarea.destroy', $tarea)}}" method="post">
+                @csrf
+                @method('delete')
+                <button class= "btn btn-danger btn-sm" type="submit">
+                    Borrar
+                </button>
+            </form>
+        </div>
+    </div>
+@endsection
